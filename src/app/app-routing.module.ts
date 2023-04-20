6004992999871271f0f23319cf0eaecd35048f57
unknown <sohithdram07@gmail.com>
2023-04-20 10:10:46 +0530

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { OurservicesComponent } from './pages/ourservices/ourservices.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { CareersComponent } from './pages/careers/careers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'home',component:HomeComponent},
+{path:'contact',component:ContactusComponent},
+{path:'services',component:OurservicesComponent},
+{path:'aboutus',component:AboutusComponent},
+{path:'careers',component:CareersComponent},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { OurservicesComponent } from './pages/ourservices/ourservices.component';
+import { CareersComponent } from './pages/careers/careers.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutusComponent,
+    OurservicesComponent,
+    CareersComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
